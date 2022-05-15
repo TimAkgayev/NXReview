a7d42d56d2e86f81d805a99c75653a6de131c962
Tim Akgayev <tim@tidus.com>
2022-05-15 18:59:27 -0400

    Changed settings

--- base_nxreview/settings.py
+++ base_nxreview/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-django_heroku.settings(locals())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [
     'D:/Documents/DjangoProjects/NXReview/nxreview/static/nxreview/',
 ]
 
+
+django_heroku.settings(locals())
